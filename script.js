10b9f0bbd29b55e8ba35b87ddd3159439e0de49c
ThomasDuple <ThomasDuple@gmail.com>
2019-02-01 22:57:21 +0100

    dfqdsfqdfq

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var price = (Math.floor(Math.random() * (50000 - 10000 + 1)) + 10000);
   console.log("Salut jeune tricheur !");
-  console.log("Le juste prix est de",price,"€");
+  console.log(("%cLe juste prix est de " + price + " €"), 'background-color: #2c2; color: #fff;');
 
   $('#plus,#minus,#proptxt,#b2').hide();
   console.log("Élements cachés");
@@ -18,18 +18,18 @@ $(document).ready(function() {
       $("#proptxt").text("que " + prop);
       $("#plus").show();
       $("#minus").hide();
-      console.log("Plus que " + prop);
+      console.log("%cPlus que " + prop, 'background-color: #22c; color: #fff;');
     };
     if (prop > price) {
       $("#proptxt").text("que " + prop);
       $("#minus").show();
       $("#plus").hide();
-      console.log("Moins que " + prop);
+      console.log("%cMoins que " + prop, 'background-color: #c22; color: #fff;');
     };
     if (prop == price) {
       $("#b1").hide();
       $("#b2").show();
-      console.log("C'est " + prop);
+      console.log("%cC'est " + prop, 'background-color: #2c2; color: #fff;');
     };
   });
 
